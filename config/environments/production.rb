6d65ebc35f6940183e97060edf31d8af8071c511
Maxime Salomon <maxime.salomon78@gmail.com>
2014-10-20 15:10:49 -0400

    Modify config/env/prod and add config asset true line

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,16 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-
-Growthbakery::Application.configure do
-    
-   # your config settings
- 
-   config.assets.compile = true
- 
-   # your other config settings
-end
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -29,15 +19,13 @@ end
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.serve_static_assets = true
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
