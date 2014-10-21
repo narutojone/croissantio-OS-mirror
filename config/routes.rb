e97a4cdb97ef54e88f997b186be1cd8ae61f3ef8
Carlos Aguilar <caguilar@dwdandsolutions.com>
2014-10-21 02:11:53 -0600

    Adding new route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   root "pages#home"
+
+  post 'subscribe' => "pages#subscribe_to_list"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
