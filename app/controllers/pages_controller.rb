81a3f3bda467e6b6ab739f6cb156f847aa284354
Maxime Salomon <maxime.salomon78@gmail.com>
2015-02-26 00:29:30 +0000

    Activated Mailchimp

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,10 @@ class PagesController < ApplicationController
    def home
    end
 
-  def subscribe_to_list
-    email = params[:email][:address]
-    @mailchimp = Mailchimp::API.new(ENV['e9c7545f809158f28a9b319e56a9ca00-us7'])
-    # Subscribe to list
-    @mailchimp.lists.subscribe(ENV['30c2db17c3'], {'email' => email})
-  end
+   def subscribe_to_list
+   		email = params[:email][:address]
+   		@mailchimp = Mailchimp::API.new(ENV['MAILCHIMP_API_KEY'])
+   		# Subscribe to list
+   		@mailchimp.lists.subscribe(ENV['MAILCHIMP_LIST_ID'], {'email' => email})
+   	end
 end
