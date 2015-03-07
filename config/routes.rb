0b4356607dafe89233f53ecbd269f199a0b75f7a
Maxime Salomon <maxime.salomon78@gmail.com>
2015-03-07 22:52:48 +0000

    Create Newsletter Page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root "pages#home"
 
   post 'subscribe' => "pages#subscribe_to_list"
+  get 'newsletter' => "pages#newsletter"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
