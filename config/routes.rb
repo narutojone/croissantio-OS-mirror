f84b8f3a1b8de2f5ca1b22eff8577a1d1d63b518
Maxime Salomon <maxime.salomon78@gmail.com>
2015-03-08 00:42:55 +0000

    Add Recrutement, Consulting and Team pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
 
   root "pages#home"
 
-  post 'subscribe' => "pages#subscribe_to_list"
   get 'newsletter' => "pages#newsletter"
+  get 'consulting' => "pages#consulting"
+  get 'recrutement' => "pages#recrutement"
+  get 'team' => "pages#team"
+
+  post 'subscribe' => "pages#subscribe_to_list"
 
 
   # The priority is based upon order of creation: first created -> highest priority.
