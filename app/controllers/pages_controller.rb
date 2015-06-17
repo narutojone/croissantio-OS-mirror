8307682df6727f485df7c11b044e9449e8d48437
Maxime Salomon <maxime.salomon78@gmail.com>
2015-06-17 13:03:41 +0200

    Subscribe with Jquery

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,11 +4,4 @@ class PagesController < ApplicationController
 
    def newsletter
    end
-
-   def subscribe_to_list
-   		email = params[:email][:address]
-   		@mailchimp = Mailchimp::API.new(ENV['MAILCHIMP_API_KEY'])
-   		# Subscribe to list
-   		@mailchimp.lists.subscribe(ENV['MAILCHIMP_LIST_ID'], {'email' => email})
-   	end
 end
