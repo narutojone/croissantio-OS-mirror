92d6e594872207607fab7a27d741c1d53e548705
Maxime Salomon <maxime.salomon78@gmail.com>
2015-06-19 23:44:39 +0200

    added NL 06-13

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get 'apprendre-cmo-slack' => "pages#apprendre-cmo-slack"
 
+  get 'experience-developpeur-mailjet' => "pages#experience-developpeur-mailjet"
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
