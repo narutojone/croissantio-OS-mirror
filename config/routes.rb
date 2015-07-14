0c052b360be1d84cb2a43e2214aecf270d0f8d1f
Maxime Salomon <maxime.salomon78@gmail.com>
2015-07-14 21:47:09 +0200

    NL 15 Juillet

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
 
   get 'alex-schultz-vp-growth-facebook' => "pages#alex-schultz-vp-growth-facebook"
 
+  get 'culture-data-zynga' => "pages#culture-data-zynga"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
