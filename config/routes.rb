d2ab9993635be65515f487e6748c5d9d9515ea8d
Maxime Salomon <maxime.salomon78@gmail.com>
2015-07-18 01:13:10 +0200

    NL 19 Juillet

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
 
   get 'culture-data-zynga' => "pages#culture-data-zynga"
 
+  get 'non-growth-hacking' => "pages#non-growth-hacking"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
