4e416a035b6e3474b2348329c08337d468114841
Maxime Salomon <maxime.salomon78@gmail.com>
2015-08-02 19:33:16 +0200

    add NL 02 aout

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,8 @@ Rails.application.routes.draw do
 
   get 'mauvaise-retention' => "pages#mauvaise-retention"
 
+  get 'from-zero-to-one-point-two' => "pages#from-zero-to-one-point-two"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
