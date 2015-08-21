ba8ff802b3d0663385853b22e7f1ae19bc96c4e9
Maxime Salomon <maxime.salomon78@gmail.com>
2015-08-21 08:40:21 +0200

    NL 23 aout draft

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,8 @@ Rails.application.routes.draw do
 
   get 'produit-levier-croissance' => "pages#produit-levier-croissance"
 
+  get 'dette-analytics' => "pages#dette-analytics"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
