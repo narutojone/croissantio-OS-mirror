6ecdc5a9297e2c973e4a4da52e405643dbfadb0b
Maxime Salomon <maxime.salomon78@gmail.com>
2015-10-04 20:14:18 +0200

    NL 4 Octobre Draft

--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,9 @@ Rails.application.routes.draw do
   get 'dette-analytics' => "pages#dette-analytics"
 
   get '16-metriques-startups' => "pages#16-metriques-startups"
+
+  get 'vraie-strategie-croissance' => "pages#vraie-strategie-croissance"
+  
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
