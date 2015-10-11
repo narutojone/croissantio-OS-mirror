351c53aa3ddd55ab16362ab3f8c7cc635b5721f7
Maxime Salomon <maxime.salomon78@gmail.com>
2015-10-11 21:47:45 +0200

    NL 11 Octobre draft

--- config/routes.rb
+++ config/routes.rb
@@ -34,8 +34,9 @@ Rails.application.routes.draw do
   get '16-metriques-startups' => "pages#16-metriques-startups"
 
   get 'vraie-strategie-croissance' => "pages#vraie-strategie-croissance"
-  
-  
+
+  get 'utilisation-feedbacks-slack' => "pages#utilisation-feedbacks-slack"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
