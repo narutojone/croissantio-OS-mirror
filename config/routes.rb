fccaeead93a8ba0f5de37915913665674ed8ef2d
Maxime Salomon <maxime.salomon78@gmail.com>
2015-10-16 15:08:23 +0200

    NL 18 Octobre

--- config/routes.rb
+++ config/routes.rb
@@ -37,6 +37,8 @@ Rails.application.routes.draw do
 
   get 'utilisation-feedbacks-slack' => "pages#utilisation-feedbacks-slack"
 
+  get 'brian-balfour-discute-growth-marketing' => "pages#brian-balfour-discute-growth-marketing"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
