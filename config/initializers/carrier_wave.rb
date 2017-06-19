f3c71be9e77fdfc10761fee592ce625e1db4e911
superbialux <superbia.lux@gmail.com>
2017-06-19 11:54:21 +0400

    Fixed fog

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,6 +2,7 @@ if Rails.env.production?
   require 'fog/aws'
   require 'carrierwave'
   CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       # Configuration for Amazon S3
       provider: 'AWS',
