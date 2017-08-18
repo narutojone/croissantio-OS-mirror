6027a128832c0323788c69b692d10c0ecb1a9a7b
superbialux <superbia.lux@gmail.com>
2017-08-18 19:57:54 +0400

    Deleted dotenv

--- config/application.rb
+++ config/application.rb
@@ -5,8 +5,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
-Dotenv.load('bot.env')
 
 module Growthbakery
   class Application < Rails::Application
