0f1debb927548f17a36165075a05824765452026
superbialux <superbia.lux@gmail.com>
2017-08-18 11:29:38 +0400

    Forgot to precompile once again

---
+++ public/assets/dropdown-53c263d4c206e31655afb40d2d4267569ae367337c11c59278770fbba153b999.js
@@ -0,0 +1,7 @@
+$('.js--res-dropdown').on('click', function() {
+  $('.res-dropdown').show("fast")
+  $(".main-container").mouseenter(function() {      
+    $('.res-dropdown').hide("fast");
+});
+})
+;
