a0dce23dae0b9c660c7419b8cb5a782bfdfa2137
superbialux <superbia.lux@gmail.com>
2017-08-18 20:44:44 +0400

    Tried to configure bot

--- config/application.rb
+++ config/application.rb
@@ -22,8 +22,8 @@ module Growthbakery
     # Set errors to custom routes
     config.exceptions_app = routes
     # Facebook bot directory
-    config.paths.add File.join("app", "bot"), glob: File.join("**","*.rb")
-    config.autoload_paths += Dir[Rails.root.join("app", "bot", "*")]
+    config.paths.add File.join('app', 'bot'), glob: File.join('**', '*.rb'), eager_load: true
+    config.autoload_paths += Dir[Rails.root.join('app', 'bot', '*')]
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
