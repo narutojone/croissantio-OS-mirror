08701e00cabe9215492ee10f9f5bb66fb5417d4e
superbialux <superbia.lux@gmail.com>
2017-08-24 14:40:43 +0400

    Trying to fix carrierwave issue once again 3

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,8 @@ module Growthbakery
     config.exceptions_app = routes
     # Facebook bot directory
     config.paths.add File.join('app', 'bot'), glob: File.join('**', '*.rb')
-    config.eager_load_paths += Dir[Rails.root.join('app', 'bot', '**', '*.rb')]
+    config.enable_dependency_loading = true
+    config.autoload_paths += Dir[Rails.root.join('app', 'bot', '**', '*.rb')]
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
