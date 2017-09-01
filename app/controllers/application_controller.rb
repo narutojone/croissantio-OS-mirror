6a3177578393b4ff52817107cb3993c264472406
superbialux <superbia.lux@gmail.com>
2017-09-01 20:58:32 +0400

    Now?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   include ApplicationHelper
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery prepend: true, with: :null_session
+  protect_from_forgery unless: -> { request.format.json? }
   def after_sign_in_path_for(_resource)
     admin_path
   end
